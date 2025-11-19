2965539a448cb45c9bfd762261a36131531d24d2
ManishaChauhan2911 <chauhanmani265@gmail.com>
2025-11-19 17:34:40 +0530

    JWT & Cookie

--- src/app.js
+++ src/app.js
@@ -4,10 +4,13 @@ const app = express();
 const User = require("./models/user");
 const {validateSignUpData} = require("./utils/validation");
 const bcrypt = require("bcrypt");
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
 
 app.use(express.json());
+app.use(cookieParser());
 
-app.post("/signup" , async (req, res) => {
+app.post("/signup" , async (req, res) => { 
    
     try{
         //Validation of data
@@ -46,17 +49,52 @@ app.post("/login", async(req , res) => {
         const isPasswordValid = await bcrypt.compare(password, user.password);
 
         if(isPasswordValid){
+            //Create a JWT token
+            const token = await jwt.sign({_id: user._id } , "DEV@Tinder$790");
+            console.log(token);
+
+            //Add  the token to cookie  and send the respones bake to the user
+            
+            res.cookie("token", token);
+
             res.send("Login Successfully!");
         }
         else{
             throw new Error("Password is not correct!");
         }
     }catch(err){
-        res.status(400).send("  ERRO:" + err.message);
+        res.status(400).send("  ERROR:" + err.message);
     }
     
 });
 
+app.get("/profile", async(req, res) => {
+    try{
+        const cookies = req.cookies;
+        const {token} = cookies;
+        if(!token){
+            throw new Error("Invalid Token");
+        }
+
+        // validate my token 
+
+        const decodedMessage = await jwt.verify(token, "DEV@Tinder$790");
+        
+        const{ _id} = decodedMessage;
+        console.log("Logged In user is: " + _id);
+
+        const user = await User.findById(_id);
+        
+        if(!user){
+            throw new Error("User does not exist!");
+        }
+
+        res.send(user);
+   }catch(err){
+    res.status(400).send("  ERROR:" + err.message);
+   }
+
+});
 
 // Get user by emailId
 app.get("/user", async (req, res) => {
@@ -93,6 +131,14 @@ app.patch("/user", async(req, res) => {
     }
 });
 
+app.get("/feed", async(req, res) => {
+    try{
+        const users = await User.find({});
+        res.send(users);
+    }catch(err){
+       res.status(400).send("something went wrong"); 
+    }
+});
 
 connectDB()
     .then(() => {
