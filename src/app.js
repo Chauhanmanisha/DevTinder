65c6f850ce6efcc00aee20d38108ad8dcba5dff8
ManishaChauhan2911 <chauhanmani265@gmail.com>
2025-11-19 18:27:43 +0530

    middelware Auth

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const {validateSignUpData} = require("./utils/validation");
 const bcrypt = require("bcrypt");
 const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
+const {userAuth} = require("./middlewares/auth");
 
 app.use(express.json());
 app.use(cookieParser());
@@ -68,32 +69,22 @@ app.post("/login", async(req , res) => {
     
 });
 
-app.get("/profile", async(req, res) => {
+app.get("/profile", userAuth, async(req, res) => {
     try{
-        const cookies = req.cookies;
-        const {token} = cookies;
-        if(!token){
-            throw new Error("Invalid Token");
-        }
-
-        // validate my token 
-
-        const decodedMessage = await jwt.verify(token, "DEV@Tinder$790");
-        
-        const{ _id} = decodedMessage;
-        console.log("Logged In user is: " + _id);
-
-        const user = await User.findById(_id);
-        
-        if(!user){
-            throw new Error("User does not exist!");
-        }
-
+        const user = req.user;
         res.send(user);
    }catch(err){
     res.status(400).send("  ERROR:" + err.message);
    }
 
+});
+
+app.post("/sendConnectionRequest", userAuth, async(req, res) => {
+    const user = req.user;
+    //Sending a connection Request
+    console.log("Sending a connection Request");
+
+    res.send(user.firstName + "Connection Request Send");
 });
 
 // Get user by emailId
---
+++ src/middlewares/auth.js
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken");
+const { model } = require("mongoose");
+const User = require("../models/user");
+
+const userAuth = async (req, res, next) => {
+    try{
+        const {token} = req.cookies;
+        if(!token){
+            throw new Error("Token is not valid!!!!!");
+        }
+
+        // validate my token 
+        const decodedMessage = await jwt.verify(token, "DEV@Tinder$790");
+        const{ _id} = decodedMessage;
+
+        const user = await User.findById(_id);
+        if(!user){
+            throw new Error("User not found");
+        }
+
+        req.user = user;
+        next();
+   }catch(err){
+    res.status(400).send("  ERROR:" + err.message);
+   }
+};
+
+module.exports = { userAuth };
